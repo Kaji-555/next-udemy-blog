f5783580ceeffffd1dda8320b0328de79e4dcbce
Kaji-555 <shutakaji2002@gmail.com>
2026-06-24 19:16:08 +0900

    実験

---
+++ src/components/auth/LoginForm.tsx
@@ -0,0 +1,53 @@
+'use client';
+import {
+    Card,
+    CardContent,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { useActionState } from 'react';
+import { authenticate } from '@/lib/actions/authenticate';
+import { useSearchParams } from 'next/navigation';
+
+export default function LoginForm() {
+    const searchParams = useSearchParams();
+    const callbackUrl = searchParams.get('callbackUrl') || '/dashboard';
+    const [errorMessage, formAction, isPending] = useActionState(
+        authenticate,
+        undefined,
+    )
+
+    return (
+        <Card className="w-full max-w-md mx-auto">
+            <CardHeader>
+                <CardTitle>ログイン</CardTitle>
+            </CardHeader>
+            <CardContent>
+                <form action={formAction} className="space-y-4">
+                    <input type="hidden" name="redirectTo" value={callbackUrl} />
+                    <div className="space-y-2">
+                        <Label htmlFor="email">メールアドレス</Label>
+                        <Input id="email" type="email" name="email" required disabled={isPending} />
+                    </div>
+                    <div className="space-y-2">
+                        <Label htmlFor="password">パスワード</Label>
+                        <Input id="password" type="password" name="password" required disabled={isPending} />
+                    </div>
+                    <Button type="submit" className="w-full" disabled={isPending}>{isPending ? "ログイン中..." : "ログイン"}</Button>
+                    <div
+                        className="flex h-8 items-end space-x-1"
+                    >
+                        {errorMessage && (
+                            <div className="text-red-500">
+                                <p className="text-sm text-red-500">{errorMessage}</p>
+                            </div>
+                        )}
+                    </div>
+                </form>
+            </CardContent>
+        </Card>
+    )
+}
